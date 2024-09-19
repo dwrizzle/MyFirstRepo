b6b5943339b62a3a8b0b2fcfc4a40796c5f0c36c
dwrizzle <desiree.wright@my.metrostate.edu>
2024-09-19 00:37:14 +0000

    why

--- HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91cf2c979c6a15860f43479751456d563c1b0444")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c8da1e06511acdd3080e6741cfc5570ea9b6a27")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
